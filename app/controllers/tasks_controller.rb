5eb5dc8f1ae11bfcd0651b78705b828e730122f0
David Kloeren <d.kloeren@googlemail.com>
2021-05-18 14:47:27 +0200

    until checkboxes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,56 @@
 class TasksController < ApplicationController
+  before_action :load_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+    # if
+    #   # reroute to index
+    #   redirect_to :tasks_path
+    # else
+    #   # display new
+    #   render :new
+    # endsd
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)  # id is determined automatically
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def load_task
+    @task = Task.find(params[:id])
+  end
+
+  # Check if params for Task.new contains exactly the required fields .. security check
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'tasks#index'
+
+  # # read
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # create
+  # get 'task/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # # http post request requires a url .. this url, does not have a view, but only an action.
+  # # within the action create ... the browser should be rerouted a url with a view
+
+  # # edit
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # remove
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
+
+  resources :tasks #, only: [:create, :index, :destroy]
+
 end
